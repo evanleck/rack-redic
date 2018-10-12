a12297fbdbfb4b1a066c76d1dc831dae6f11237b
Evan Lecklider <evan@lecklider.com>
2018-10-11 18:01:47 -0700

    Cleanup from Rubocop

--- test/helper.rb
+++ test/helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Add our project folder to the root of our load path.
-$LOAD_PATH.unshift File.expand_path('../..', __FILE__)
+$LOAD_PATH.unshift File.expand_path('..', __dir__)
 
 # Ensure we have this set before trying to initialize anything.
 ENV['REDIS_URL'] ||= 'redis://localhost:6379'
